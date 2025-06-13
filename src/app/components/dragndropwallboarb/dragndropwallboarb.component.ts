e72156182fe3d7432f32c54a17b75ba21879941f
aayush-ak912 <polyurethane.ppu@gmail.com>
2025-06-13 09:09:52 +0530

    components added

---
+++ src/app/components/dragndropwallboarb/dragndropwallboarb.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragndropwallboarb',
+  imports: [],
+  templateUrl: './dragndropwallboarb.component.html',
+  styleUrl: './dragndropwallboarb.component.css'
+})
+export class DragndropwallboarbComponent {
+
+}
