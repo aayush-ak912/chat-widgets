fb3b5bdd09dc31f91c51f9319872871fb9a1464d
grabcoding <programmers472@gmail.com>
2025-06-14 09:45:53 +0530

    bur resolved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 import { LayoutsComponent } from "./components/layouts/layouts.component";
 import { DragndropwallboarbComponent } from "./components/dragndropwallboarb/dragndropwallboarb.component";
 import { DragndropskinComponent } from "./components/dragndropskin/dragndropskin.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LayoutsComponent, DragndropwallboarbComponent, DragndropskinComponent],
+  imports: [LayoutsComponent, DragndropwallboarbComponent, DragndropskinComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
